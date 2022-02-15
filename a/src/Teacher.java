67d070e95dfb6298b1c5766086ea14a0dc71c860
sjack5354 <1462813013@qq.com>
2022-02-15 10:29:31 +0800

    分支zhangsan设置了lisi睡觉的方法

--- a/src/Teacher.java
+++ a/src/Teacher.java
@@ -2,4 +2,8 @@ public class Teacher {
     private String name = "lisi";
     private Integer age = 18;
     private String sex = "男";
+
+    public void sleep(){
+        System.out.println("this is lisi sleep method");
+    }
 }
