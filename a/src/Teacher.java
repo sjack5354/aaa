9e046f71a3e8e3bff44fae4d8c9df69dd2360bf5
sjack5354 <1462813013@qq.com>
2022-02-15 10:25:31 +0800

    分支lisi设置了吃饭方法

--- a/src/Teacher.java
+++ a/src/Teacher.java
@@ -2,4 +2,10 @@ public class Teacher {
     private String name = "lisi";
     private Integer age = 18;
     private String sex = "男";
+
+    public void eat(){
+        System.out.println("this is lisi eat method");
+    }
 }
+
+
